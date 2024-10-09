c53ee23f2ed2c9df386020f2983eb534cca81c4e
merliariza <180667185+merliariza@users.noreply.github.com>
2024-10-08 23:17:52 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,82 +3,115 @@ import json
 import modules.menu as mn
 import modules.mensajes as msg
 import modules.nickname as nn
-if (__name__ == "__main__"):
-    
-    isActive = True
-    chachipun = {}
+import modules.validate as val
+import modules.datos as da
+import modules.estadisticas as es
+from modules import historial as hi
 
-    while (isActive):
+if (__name__ == "__main__"):
+
+    isActive = True
+    
+    while isActive:
         try:
             os.system('cls')
             print(msg.titulo)
             print(mn.menu)
-            opcMenu = int(input(':)_'))
+            opcMenu = int(input(':)_')) #Permite digitar una opción del menú inicial.
             
             match opcMenu:
                 case 1:
                     isIngresar = True
-                    while (isIngresar):
+                    while isIngresar:
                         try:
                             os.system('cls')
                             print(msg.tituloIngresar)
                             print(mn.menuIngresar)
-                            opcIngresar = int(input(':)_'))
+                            opcIngresar = int(input(':)_')) #Permite digitar una opción del menú ingresar.
                             
                             match opcIngresar:
-                                case 1:
-                                    print(nn.ingresarUno)
+                                case 1:  
+                                    historial = nn.cargarHistorial() #Carga el historial dentro de la función historial.
+                                    nn.ingresarUno(historial) #Ingresa a un usuario y juega vs pc.
+                                    da.jugarConEscudoPC(historial) #Valida si hay un escudo y lo pone en funcionamiento.
                                     os.system('pause')
-                                case 2:
-                                    print(nn.ingresarDos)
-                                    os.system('pause')
+
+                                case 2: 
+                                    historial = nn.cargarHistorial() #Carga el historial dentro de la función historial.
+                                    resultado = nn.ingresarDos(historial) #Carga el ingreso de juegos y partida Uno vs Uno
+                                    if resultado is not None: #Si el resultado no pertenece a None, significa que se guardaron
+                                        nickname1, nickname2 = resultado 
+                                        da.jugarConEscudoDosJugadores(nickname1, nickname2)
+                                case 3:
+                                    isActive = val.validateReturn(msg.msgVolver)
+                                    if isActive:
+                                        isIngresar = False  
+                                    else:
+                                        isIngresar = True
                         except ValueError:
                             print('error en el dato ingresado')
                             os.system ('pause')
                 case 2:
                     isHistorial = True
-                    while (isHistorial):
+                    while isHistorial:
                         try:
                             os.system('cls')
                             print(msg.tituloHistorial)
                             print(mn.menuHistorial)
-                            match opcIngresar:
+                            opcHistorial = int(input(':)_')) #Permite digitar una opción del menú Historial.
+
+                            match opcHistorial:
                                 case 1:
-                                    print()
+                                    hi.mostrarHistorialJugadores() #Carga el historial de jugadores y sus roles.
                                     os.system('pause')
                                 case 2:
-                                    print()
+                                    hi.mostrarTablaPuntos() #Carga el tablero de puntos por jugador ingresado.
                                     os.system('pause')
                                 case 3:
-                                    print()
+                                    hi.mostrarHistorialIA() #Muestra el historial de juego de la IA
                                     os.system('pause')
+                                case 4: 
+                                    isActive = val.validateReturn(msg.msgVolver)
+                                    if isActive:
+                                        isHistorial = False  
+                                    else:
+                                        isHistorial = True
                         except ValueError:
                             print('error en el dato ingresado')
                             os.system ('pause')
                 case 3: 
                     isEstadisticas=True
-                    while (isEstadisticas):
+                    while isEstadisticas:
                         try:
                             os.system('cls')
-                            print(msg.tituloHistorial)
-                            print(mn.menuHistorial)
-                            match opcIngresar:
+                            print(msg.tituloEstadisticas)
+                            print(mn.menuEstadisticas)
+                            opcEstadisticas = int(input(':)_')) #Permite digitar una opción del menú Estadisticas.
+
+                            match opcEstadisticas:
                                 case 1:
-                                    print()
+                                    es.listarTresMejores() #Carga el listado de los tres mejores jugadores.
                                     os.system('pause')
                                 case 2:
-                                    print()
+                                    es.listarUltimoPuesto() #Carga ultimo puesto por puntos.
                                     os.system('pause')
                                 case 3:
-                                    print()
+                                    es.listarPerdedoresContraIA() #Carga perdedores contra IA.
                                     os.system('pause')
                                 case 4:
-                                    print()
+                                    es.promedioJugadoresGanaronIA() #Carga promedio de ganadores vs IA
                                     os.system('pause')
+                                case 5:
+                                    isActive = val.validateReturn(msg.msgVolver)
+                                    if isActive:
+                                        isEstadisticas = False  
+                                    else:
+                                        isEstadisticas = True
                         except ValueError:
                             print('error en el dato ingresado')
                             os.system ('pause')
+                case 4:
+                    isActive = val.validateData(msg.msgSalir)
         except ValueError:
             print('Error en el dato ingresado')
             os.system('pause') 
-    
